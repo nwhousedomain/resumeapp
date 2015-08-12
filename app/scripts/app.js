e667ce78a5659fedde3d7ead7e1b1fa0f75e42fe
Manny Maisonneuve <nwhousedomain@gmail.com>
2015-08-12 04:35:45 -0400

    adding content on the app

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,21 +15,50 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
+  .config(function ($urlRouterProvider,$stateProvider,$locationProvider) {
+      $urlRouterProvider
+        .otherwise('/');
+      //$locationProvider.html5Mode(true);
+
+      $stateProvider.state('main', {
+          url: '/main',
+          templateUrl: 'views/main.html',
+          controller: 'Main.controller',
+          //controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+           .state('main.officemax', {
+               url: '/officemax',
+               templateUrl: 'views/Shared/officemax.html',
+               //controller: 'resume.controller',
+               //controllerAs: 'main'
+           })
+        .state('main.vproductions', {
+            url: '/vproductions',
+            templateUrl: 'views/Shared/vproductions.html',
+            //controller: 'resume.controller',
+            //dcontrollerAs: 'main'
+        })
+       .state('main.uok', {
+           url: '/uok',
+           templateUrl: 'views/Shared/uok.html',
+           //controller: 'resume.controller',
+           //dcontrollerAs: 'main'
+       })
+       .state('main.proux', {
+           url: '/proux',
+           templateUrl: 'views/Shared/proux.html',
+           //controller: 'resume.controller',
+           //dcontrollerAs: 'main'
+       })
+
+        
+      $stateProvider.state('resume', {
+          url: '/resume',
+          templateUrl: 'views/resume.html',
+          //controller: 'resume.controller',
+          //controllerAs: 'main'
       });
   });
---
+++ app/scripts/controllers/main.controller.js
@@ -0,0 +1,5 @@
+angular.module('resumeappApp')
+   .controller('Main.controller', ['$scope', function ($scope) {
+
+   }]);
+
